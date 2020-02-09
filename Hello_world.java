1834577b2468462e3d7a013281370acc41a9669e
LoveJT <mengtongbao@126.com>
2020-02-09 17:47:21 +0800

    Hello

---
+++ Hello_world.java
@@ -0,0 +1,11 @@
+public class HelloWorld{
+	public static void main(String[] args){
+		System.out.println("HelloWorld");
+
+		System.out.println("H3");
+		System.out.println("1");
+
+		System.out.println("3");
+		System.out.println("2");
+	}
+}
